1e329844d66594773e0b78a053de173c0b194745
한상선 <hs94122@naver.com>
2022-05-28 20:08:14 +0900

    study 알고리즘

---
+++ src/baekjoon/step/a5.java
@@ -0,0 +1,11 @@
+package baekjoon.step;
+
+import java.util.Scanner;
+
+public class a5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(sc.nextInt() - (2541-1998));
+    }
+}
---
+++ src/baekjoon/step/a8.java
@@ -0,0 +1,20 @@
+package baekjoon.step;
+
+import java.util.Scanner;
+
+public class a8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        if (A>B){
+            System.out.println(">");
+        } else if (A < B) {
+            System.out.println("<");
+        } else {
+            System.out.println("==");
+        }
+    }
+}
