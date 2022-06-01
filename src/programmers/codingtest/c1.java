e4374b4573f5ead08e8aa1043b117a00a2766874
한상선 <hs94122@naver.com>
2022-06-01 19:20:19 +0900

    study 알고리즘

---
+++ src/programmers/codingtest/c1.java
@@ -0,0 +1,9 @@
+package programmers.codingtest;
+
+public class c1 {
+    public static void main(String[] args) {
+        //신고결과 받기
+
+
+    }
+}
---
+++ src/programmers/codingtest/skillcheck1.java
@@ -0,0 +1,16 @@
+package programmers.codingtest;
+
+public class skillcheck1 {
+
+    public static void main(String[] args) {
+        int[][] arr1 = new int[2][];
+        int[][] arr2 = new int[2][];
+        System.out.println(solution(arr1, arr2));
+    }
+
+    public static int[][] solution(int[][] arr1, int[][] arr2){
+        int [][] answer ={};
+        return answer;
+    }
+
+}
